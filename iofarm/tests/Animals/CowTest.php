c83fd47552001c510363271df33f382c98b2c856
James McGraw <james.mcgraw.allen@gmail.com>
2023-05-11 17:00:58 +0100

    Finished unit testing

--- iofarm/tests/Animals/CowTest.php
+++ iofarm/tests/Animals/CowTest.php
@@ -13,7 +13,12 @@ class CowTest extends TestCase
         $grassMock = $this->createMock(Grass::class); //Creates a new instance of Grass class as a mock
         $grassMock->expects($this->once()) // Only will need it once in this method
             ->method('getFoodType') // Will do a method called getFoodType
-            ->willReturn('grass'); // Will get back this value - so ignore it when you do the unit test.
+            ->willReturn('grass'); // Would get back this value if it was run - so use this when you do the unit test.
+        // can also use:
+        // ->expects($this->once())
+        // ->expects($this->any())
+        // ->expects($this->exactly(2))
+        // to specify how many times the method should be called
         $result = $cow->eat($grassMock);
         $this->assertEquals('Mmm grass', $result);
     }
