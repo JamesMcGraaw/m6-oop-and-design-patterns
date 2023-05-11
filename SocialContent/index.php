56ab1294b84b826a813ca93e908b63c3edb54905
James McGraw <james.mcgraw.allen@gmail.com>
2023-05-11 09:15:40 +0100

    Completed social content exercise

--- SocialContent/index.php
+++ SocialContent/index.php
@@ -23,70 +23,28 @@ $posts = $contentDAO->fetchAll();
         <div class="col-12">
 
             <?php
-            echo '<pre>';
-            var_dump($posts);
-            echo '</pre>';
-            ?>
-
-            <div class="card mb-4">
-                <div class="card-body">
-                    <h5 class="card-title">bittersweet <small class="small mr-2 float-right text-muted">14/12/2022 14:07:15</small></h5>
-                    <p class="card-text">Just took the most gorgeous sunset walk - what a way to end the day!</p>
-                    <div class="btn-group" role="group">
-                        <button type="button" class="btn btn-info">-</button>
-                        <span class="disabled btn btn-outline-info">0</span>
-                        <button type="button" class="btn btn-info">+</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card mb-4">
-                <div class="card-body">
-                    <h5 class="card-title">gregorygreg <small class="small mr-2 float-right text-muted">14/12/2022 14:05:52</small></h5>
-                    <p class="card-text">Can't believe it's already October - this year is flying by...wait it's December?!</p>
-                    <div class="btn-group" role="group">
-                        <button type="button" class="btn btn-info">-</button>
-                        <span class="disabled btn btn-outline-info">1</span>
-                        <button type="button" class="btn btn-info">+</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card mb-4">
-                <div class="card-body">
-                    <h5 class="card-title">flowerpower <small class="small mr-2 float-right text-muted">14/12/2022 14:05:09</small></h5>
-                    <p class="card-text">Picked up some fresh flowers for the house - so nice to have a little bit of nature indoors!</p>
-                    <div class="btn-group" role="group">
-                        <button type="button" class="btn btn-info">-</button>
-                        <span class="disabled btn btn-outline-info">4</span>
-                        <button type="button" class="btn btn-info">+</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card mb-4">
-                <div class="card-body">
-                    <h5 class="card-title">iamsarah <small class="small mr-2 float-right text-muted">14/12/2022 14:04:50</small></h5>
-                    <p class="card-text">Brunch with friends is the best way to start the weekend</p>
-                    <div class="btn-group" role="group">
-                        <button type="button" class="btn btn-info">-</button>
-                        <span class="disabled btn btn-outline-info">5</span>
-                        <button type="button" class="btn btn-info">+</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card mb-4">
-                <div class="card-body">
-                    <h5 class="card-title">amyrobson <small class="small mr-2 float-right text-muted">14/12/2022 14:04:14</small></h5>
-                    <p class="card-text">Just finished up a great workout at the gym and feeling so energized!</p>
-                    <div class="btn-group" role="group">
-                        <button type="button" class="btn btn-info">-</button>
-                        <span class="disabled btn btn-outline-info">12</span>
-                        <button type="button" class="btn btn-info">+</button>
-                    </div>
-                </div>
-            </div>
+            foreach ($posts as $post) {
+            echo ('<div class="card mb-4">'
+                .'<div class="card-body">'
+                .'<h5 class="card-title">'
+                . $post->getName()
+                .'<small class="small mr-2 float-right text-muted">'
+                . $post->getCreated()
+                .'</small>'
+                .'</h5>'
+                .'<p class="card-text">'
+                . $post->getPost()
+                .'</p>'
+                .'<div class="btn-group" role="group">'
+                .'<button type="button" class="btn btn-info">-</button>'
+                .'<span class="disabled btn btn-outline-info">'
+                . $post->getUpvotes()
+                .'</span>'
+                .'<button type="button" class="btn btn-info">+</button>'
+                .'</div>'
+                .'</div>'
+            .'</div>');
+        }?>
 
         </div>
     </div>
